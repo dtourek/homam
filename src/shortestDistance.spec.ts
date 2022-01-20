51c6c9b614651e354bdb87de05cf80a3c4dff4bf
davidtourek <dtourek@centrum.cz>
2022-01-20 13:07:29 +0100

    wip - shortest move refactored, tested. Before adding ancestors

--- src/shortestDistance.spec.ts
+++ src/shortestDistance.spec.ts
@@ -1,7 +1,6 @@
-import { FieldType } from './map/field';
-
-const N = 4;
-const M = 4;
+import { FieldType, isObstacleField } from './map/field';
+import { WorldMap } from './store';
+import { ILocation } from './types';
 
 interface IItem {
   row: number;
@@ -11,25 +10,20 @@ interface IItem {
 
 const createItem = (row: number, col: number, dist: number): IItem => ({ row, col, dist });
 
-function minDistance(grid: string[][]) {
-  const player = createItem(0, 0, 0);
+function minDistance(map: WorldMap, playerLocation: ILocation, destination: ILocation) {
+  const player = createItem(playerLocation.y, playerLocation.x, 0);
+  const maxWidth = map[0].length;
+  const maxHeight = map.length;
 
   // To keep track of visited QItems. Marking
   // blocked cells as visited.
-  const visited = Array.from(Array(N), () => Array(M).fill(0));
-  console.log(visited);
-  grid.forEach((row, i) => {
+  const visited = Array.from(Array(maxHeight), () => Array(maxWidth).fill(0));
+  map.forEach((row, i) => {
     row.forEach((_field, j) => {
-      if (grid[i][j] === '0') {
+      if (isObstacleField(map[i][j])) {
         visited[i][j] = true;
       } else {
         visited[i][j] = false;
-      }
-
-      // Finding source
-      if (grid[i][j] === 's') {
-        player.row = i;
-        player.col = j;
       }
     });
   });
@@ -38,12 +32,13 @@ function minDistance(grid: string[][]) {
   const fields = [];
   fields.push(player);
   visited[player.row][player.col] = true;
+
   while (fields.length != 0) {
     const first: IItem = fields[0];
     fields.shift();
 
     // Destination found;
-    if (grid[first.row][first.col] === 'd') {
+    if (first.row === destination.y && first.col === destination.x) {
       return first.dist;
     }
 
@@ -54,7 +49,7 @@ function minDistance(grid: string[][]) {
     }
 
     // moving down
-    if (first.row + 1 < N && visited[first.row + 1][first.col] === false) {
+    if (first.row + 1 < maxHeight && visited[first.row + 1][first.col] === false) {
       fields.push(createItem(first.row + 1, first.col, first.dist + 1));
       visited[first.row + 1][first.col] = true;
     }
@@ -66,7 +61,7 @@ function minDistance(grid: string[][]) {
     }
 
     // moving right
-    if (first.col + 1 < M && visited[first.row][first.col + 1] === false) {
+    if (first.col + 1 < maxWidth && visited[first.row][first.col + 1] === false) {
       fields.push(createItem(first.row, first.col + 1, first.dist + 1));
       visited[first.row][first.col + 1] = true;
     }
@@ -74,31 +69,74 @@ function minDistance(grid: string[][]) {
   return -1;
 }
 
-const grid = [
-  ['*', '*', '*', '0'],
-  ['s', '0', '*', '*'],
-  ['0', '0', '0', '*'],
-  ['d', '*', '*', '*'],
-];
-
-const map = [
-  [{ type: FieldType.forest }, { type: FieldType.mountain }],
-  [{ type: FieldType.grass }, { type: FieldType.mud }],
-];
-
 describe('shortestDistance', () => {
   it('should return -1 when path does not exist', () => {
     expect(
-      minDistance([
-        ['0', '*', '0', '0'],
-        ['0', '*', '0', '0'],
-        ['s', '*', '0', '0'],
-        ['0', '0', '0', 'd'],
-      ]),
+      minDistance(
+        [
+          [{ type: FieldType.grass }, { type: FieldType.mountain }],
+          [{ type: FieldType.forest }, { type: FieldType.mountain }],
+        ],
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+      ),
     ).toEqual(-1);
   });
 
-  it('should work', () => {
-    expect(minDistance(grid)).toEqual(10);
+  it('should work in mud fields', () => {
+    expect(minDistance([[{ type: FieldType.mud }, { type: FieldType.mud }]], { x: 0, y: 0 }, { x: 1, y: 0 })).toEqual(1);
+  });
+
+  it('should work on grass fields', () => {
+    expect(minDistance([[{ type: FieldType.grass }, { type: FieldType.grass }]], { x: 0, y: 0 }, { x: 1, y: 0 })).toEqual(1);
+  });
+
+  it('should return -1 when mountain field makes destination unreachable', () => {
+    expect(minDistance([[{ type: FieldType.grass }, { type: FieldType.mountain }]], { x: 0, y: 0 }, { x: 1, y: 0 })).toEqual(-1);
+  });
+
+  it('should return -1 when forest field makes destination unreachable', () => {
+    expect(minDistance([[{ type: FieldType.grass }, { type: FieldType.forest }]], { x: 0, y: 0 }, { x: 1, y: 0 })).toEqual(-1);
+  });
+
+  it('should return -1 when water field makes destination unreachable', () => {
+    expect(minDistance([[{ type: FieldType.grass }, { type: FieldType.water }]], { x: 0, y: 0 }, { x: 1, y: 0 })).toEqual(-1);
+  });
+
+  // TODO - fix it? Player should never be on obstacle...
+  xit('should return -1 when player start is on mountain field', () => {
+    expect(minDistance([[{ type: FieldType.mountain }, { type: FieldType.grass }]], { x: 0, y: 0 }, { x: 1, y: 0 })).toEqual(-1);
+  });
+
+  it('should work on complex map full of grass fields', () => {
+    expect(
+      minDistance(
+        [
+          [{ type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }],
+        ],
+        { x: 0, y: 0 },
+        { x: 4, y: 4 },
+      ),
+    ).toEqual(8);
+  });
+
+  it('should work on complex map with obstacles', () => {
+    expect(
+      minDistance(
+        [
+          [{ type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }],
+          [{ type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.grass }, { type: FieldType.mountain }, { type: FieldType.grass }],
+        ],
+        { x: 0, y: 0 },
+        { x: 4, y: 4 },
+      ),
+    ).toEqual(16);
   });
 });
