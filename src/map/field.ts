55e2930bd71966ffdf74ef878b961569a9dd38c4
David Tourek <32526698+dtourek@users.noreply.github.com>
2022-02-20 12:07:59 +0100

    Shortest map move (#4)
    * wip - shortest move
    
    * wip - shortest move refactored, tested. Before adding ancestors
    
    * feat - dijkstra algo implemented to find shortest path

--- src/map/field.ts
+++ src/map/field.ts
@@ -8,6 +8,8 @@ export enum FieldType {
   forest = 'forest',
   grass = 'grass',
   mud = 'mud',
+  swamp = 'swamp',
+  desert = 'desert',
 }
 
 export const isObstacleField = (field: IField): boolean => field.type === FieldType.water || field.type === FieldType.forest || field.type === FieldType.mountain;
