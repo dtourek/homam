7c258092fb29d9992f39cf90a1a19c3145baba71
David Tourek <32526698+dtourek@users.noreply.github.com>
2023-01-24 22:34:19 +0100

    feat[map, field] strucuture consolidated (#84)

--- next/src/modules/map/Map.tsx
+++ next/src/modules/map/Map.tsx
@@ -1,25 +1,15 @@
 import React, {useContext, useEffect, useState} from "react";
 import {ConfigContext} from "homam/modules/config/store";
-import {getRawFields} from "homam/modules/field/getRawFields";
 import {Field} from "homam/modules/field/Field";
 
 export const Map = () => {
   const config = useContext(ConfigContext)
 
-  const [fields, setFields] = useState(getRawFields(config));
-
-  useEffect(() => {
-    setFields(getRawFields(config));
-  }, [config]);
-
   const onFieldClick = (x: number, y: number) => {
     console.log('CLICKED ON:', x, y)
   }
 
-
   return <svg xmlns="http://www.w3.org/2000/svg" height={config.map.maxSize} width={config.map.maxSize}>
-    {fields.map(({ x, y, ...props }) => (
-      <Field key={`field-${x},${y}`} {...props} x={x * config.map.fieldSize} y={y * config.map.fieldSize} onClick={() => onFieldClick(x, y)} />
-    ))}
+    {config.map.fields.map((row, x) => row.flatMap((field, y) => <Field key={`field-${x},${y}`} fieldSize={config.map.fieldSize} field={field} x={x} y={y} onClick={() => onFieldClick(x, y)} />))}
   </svg>
 }
