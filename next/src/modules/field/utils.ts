31672d97706d4242c644321594c340a08fefbec8
davidtourek <dtourek@centrum.cz>
2023-01-27 23:00:27 +0100

    feat[map, hero] hero being rendered in a map

--- next/src/modules/field/utils.ts
+++ next/src/modules/field/utils.ts
@@ -1,4 +1,4 @@
-import {FieldObjectType, FieldType, IDesertField, IGrassField, IMountainField, IResourceObject, Resource} from "homam/modules/field/interfaces";
+import {FieldObjectType, FieldType, IDesertField, IGrassField, IHeroFieldObject, IMountainField, IResourceObject, Resource} from "homam/modules/field/interfaces";
 
 export const desertField: IDesertField = { type: FieldType.desert, weight: 2, color: '#E7E4A5', isObstacle: false }
 export const mountainField: IMountainField = { type: FieldType.mountain, color: '#1C1C1D', isObstacle: true }
@@ -6,3 +6,4 @@ export const grassField: IGrassField = { type: FieldType.grass,  weight: 1, colo
 
 export const goldResourceField: IResourceObject  = { type: FieldObjectType.Resource, object: { amount: 1000, type: Resource.Gold, color: '#FFD700' } }
 export const woodResourceField: IResourceObject  = { type: FieldObjectType.Resource, object: { amount: 10, type: Resource.Wood, color: '#603C1A' } }
+export const heroPlayer1Field: IHeroFieldObject = { type: FieldObjectType.Hero, object: { color: 'blue', name: 'Player 1' }}
