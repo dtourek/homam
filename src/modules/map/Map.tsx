898af7c3fd51c94351ecbea88902483076b8d6db
Honza Smid <jansmidhonza@gmail.com>
2023-02-02 23:33:07 +0100

    better real location function (#91)
    * better real location function
    
    * lint
    
    * rebased
    
    * lint
    
    * fixes
    
    * ci fix
    
    ---------
    
    Co-authored-by: davidtourek <dtourek@centrum.cz>

--- src/modules/map/Map.tsx
+++ src/modules/map/Map.tsx
@@ -4,6 +4,7 @@ import { Hero } from 'homam/modules/hero/Hero';
 import { Cursor } from 'homam/modules/cursor/Cursor';
 import { Fields } from 'homam/modules/field/Fields';
 import { cursorMoveAction, heroMoveAction } from 'homam/modules/store/actions';
+import { locationFromMouseEvent } from 'homam/modules/utils';
 
 export const Map = () => {
   const store = useContext(GameStore);
@@ -11,42 +12,17 @@ export const Map = () => {
   const element = useRef<SVGSVGElement>(null);
 
   const onMouseMove = (event: MouseEvent<SVGSVGElement>): void => {
-    if (!element.current) {
-      return;
-    }
-
-    const bounding = element.current.getBoundingClientRect();
-
-    const mapXPosition = event.clientX - bounding.left;
-    const mapYPosition = event.clientY - bounding.top;
-    // right
-    if (mapXPosition - store.cursor.location.x > store.map.fieldSize) {
-      const x = Math.round(mapXPosition / store.map.fieldSize) * store.map.fieldSize;
-      action(cursorMoveAction({ x, y: store.cursor.location.y }));
-    } else if (mapXPosition < store.cursor.location.x) {
-      // left
-      const x = Math.floor(mapXPosition / store.map.fieldSize) * store.map.fieldSize;
-      action(cursorMoveAction({ x, y: store.cursor.location.y }));
-    } else if (mapYPosition - store.cursor.location.y > store.map.fieldSize) {
-      // down
-      const y = Math.round(mapYPosition / store.map.fieldSize) * store.map.fieldSize;
-      action(cursorMoveAction({ x: store.cursor.location.x, y }));
-    } else if (mapYPosition < store.cursor.location.y) {
-      // up
-      const y = Math.floor(mapYPosition / store.map.fieldSize) * store.map.fieldSize;
-      action(cursorMoveAction({ x: store.cursor.location.x, y }));
-    }
+    const location = locationFromMouseEvent(event, store.cursor.location, store.map.fieldSize, element.current);
+    action(cursorMoveAction(location));
   };
 
-  const onClick = (event: MouseEvent<SVGSVGElement>) => {
-    // const bounding = element.current.getBoundingClientRect()
-    //todo: calculate real position
-    // hero(moveAction([event.clientX - bounding.left, event.clientY - bounding.top]))
-    action(heroMoveAction({ x: store.cursor.location.x, y: store.cursor.location.y }));
+  const onMouseDown = (event: MouseEvent<SVGSVGElement>) => {
+    const location = locationFromMouseEvent(event, store.cursor.location, store.map.fieldSize, element.current);
+    action(heroMoveAction(location));
   };
 
   return (
-    <svg ref={element} xmlns="http://www.w3.org/store.map.tileSize00/svg" height={store.map.maxSize} width={store.map.maxSize} onMouseMove={onMouseMove} onClick={onClick}>
+    <svg ref={element} xmlns="http://www.w3.org/store.map.tileSize00/svg" height={store.map.maxSize} width={store.map.maxSize} onMouseMove={onMouseMove} onMouseDown={onMouseDown}>
       <Fields />
       <Hero />
       <Cursor />
