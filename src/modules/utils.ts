9a8637474a81de9b822613bf17929559a3bd58e3
Honza Smid <jansmidhonza@gmail.com>
2023-02-02 22:07:26 +0100

    better real location function

---
+++ src/modules/utils.ts
@@ -0,0 +1,17 @@
+import {MouseEvent} from "react";
+import {ILocation} from "homam/modules/store/store";
+
+export const locationFromMouseEvent = (event: MouseEvent<SVGSVGElement>, cursor: ILocation, tileSize: number, svg: SVGElement | null): ILocation=> {
+    const bounding = svg?.getBoundingClientRect()
+    if (!bounding) {
+        return cursor
+    }
+
+    const mapXPosition = event.clientX - bounding.left;
+    const mapYPosition = event.clientY - bounding.top;
+
+    const x = Math.floor(mapXPosition / tileSize) * tileSize;
+    const y = Math.floor(mapYPosition / tileSize) * tileSize;
+
+    return { x, y }
+}
