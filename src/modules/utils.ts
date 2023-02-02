23cdd66e02190a101d556bdf435ef72252e851b1
davidtourek <dtourek@centrum.cz>
2023-02-02 23:26:24 +0100

    lint

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,12 +1,7 @@
-import { MouseEvent } from "react";
-import { ILocation } from "homam/modules/store/store";
+import { MouseEvent } from 'react';
+import { ILocation } from 'homam/modules/store/store';
 
-export const locationFromMouseEvent = (
-  event: MouseEvent<SVGSVGElement>,
-  cursor: ILocation,
-  tileSize: number,
-  svg: SVGElement | null
-): ILocation => {
+export const locationFromMouseEvent = (event: MouseEvent<SVGSVGElement>, cursor: ILocation, tileSize: number, svg: SVGElement | null): ILocation => {
   const bounding = svg?.getBoundingClientRect();
   if (!bounding) {
     return cursor;
